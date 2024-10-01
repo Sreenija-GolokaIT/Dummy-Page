30eb5838803fe883367d2db1940d1c3b39163f21
Sreenija-GolokaIT <sreenija@golokait.com>
2024-10-01 23:30:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,32 +7,27 @@ function App() {
     <div className="App">
       <JobCard
         title="Office Manager"
-        company="Exoticca"
         experience="3 years"
-        location="Barcelona, Catalonia, Spain"
-        category="People"
+        location="Mumbai"
         type="Full time"
         datePosted="1 day"
       />
       <JobCard
         title="Team lead"
         experience="2 years"
-        location="San Jacinto, California, United States"
-        category="Administrative"
+        location="Bangalore"
         type="Full time"
         datePosted="5 days"
       />
       <JobCard
         title="Software engineer intern"
-        company="Brady Martz and Associates"
         experience="5 years"
-        location="Grand Forks, North Dakota, United States"
-        type="Full time"
+        location="Chennai"
+        type="Part time"
         datePosted="8 days"
       />
       <JobCard
         title="Data Scientist Intern"
-        company="GolokaIT"
         experience="5 years"
         location="Mumbai"
         type="Full time"
@@ -40,7 +35,6 @@ function App() {
       />
       <JobCard
         title="Data Scientist Intern"
-        company="GolokaIT"
         experience="5 years"
         location="Mumbai"
         type="Full time"
